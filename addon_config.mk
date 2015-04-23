b0819c893520a0291fe42c85a018bfe9a18a0b62
Christopher Baker <me@christopherbaker.net>
2015-04-23 08:54:40 -0500

    Update json requirement.

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxSQLiteCpp
+	ADDON_DESCRIPTION = A thin wrapper for the SQLiteCpp library.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "sqlite"
+	ADDON_URL = http://github.com/bakercp/ofxSQLiteCpp
+
+common:
+	ADDON_DEPENDENCIES = ofxJSON
